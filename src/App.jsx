a61d719d0672a4daf050d56cabcaaae23b4eda98
Alankar Singh <alankar010499@gmail.com>
2025-07-10 16:20:36 +0530

    feat: add rich text editor with toolbar and document page
    - Updated package.json to include @emotion/css and is-hotkey dependencies.
    - Created Document page with rich text editing capabilities using Slate.js.
    - Implemented Toolbar and Menu components for formatting options.
    - Added Button and Icon components for UI consistency.
    - Integrated hotkey support for text formatting (bold, italic, underline, etc.).

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import LoginRegisterPage from "./pages/LoginRegisterPage";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Document from "./pages/Document";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginRegisterPage />} />
+        <Route path="/document" element={<Document />} /> 
       </Routes>
     </Router>
   );
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,19 @@
+import { css, cx } from "@emotion/css";
+import React from "react";
+import { Menu } from "./Menu";
+export const Toolbar = React.forwardRef(({ className, ...props }, ref) => (
+  <Menu
+    {...props}
+    ref={ref}
+    className={cx(
+      className,
+      css`
+        position: relative;
+        padding: 1px 18px 17px;
+        margin: 0 -20px;
+        border-bottom: 2px solid #eee;
+        margin-bottom: 20px;
+      `
+    )}
+  />
+));
